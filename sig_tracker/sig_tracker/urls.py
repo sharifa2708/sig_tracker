d877ca33a0e342146d320beb8f51ec8ba8d08f89
SHIVANGI <shivangibitsp@gmail.com>
2018-07-19 21:18:09 +0530

    Update urls.py

--- sig_tracker/sig_tracker/urls.py
+++ sig_tracker/sig_tracker/urls.py
@@ -3,10 +3,7 @@ from django.conf.urls import include,url
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-<<<<<<< HEAD
     url(r'^main/', include('main.urls')),
 ]
-=======
-    url(r'^about_sig/', include('about_sig.urls')),
-]
->>>>>>> bc743e6ad624fa4dc5c5ab6f9c56f3f6b5795605
+
+
