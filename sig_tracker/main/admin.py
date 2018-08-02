531244f4fc50a5f7af58a82ab52f9b5d7eb22c68
sharifa2708 <sanjay5803@gmail.com>
2018-08-03 02:03:10 +0530

    Removed redundant lines

--- sig_tracker/main/admin.py
+++ sig_tracker/main/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from . models import Sig
-<<<<<<< HEAD
 from . models import DevSig
 from . models import MlSig
 from . models import CpSig
@@ -9,9 +8,3 @@ admin.site.register(Sig)
 admin.site.register(DevSig)
 admin.site.register(MlSig)
 admin.site.register(CpSig)
-=======
-from . models import Topics
-
-admin.site.register(Sig)
-admin.site.register(Topics)
->>>>>>> d877ca33a0e342146d320beb8f51ec8ba8d08f89
--- sig_tracker/sig_tracker/urls.py
+++ sig_tracker/sig_tracker/urls.py
@@ -3,18 +3,6 @@ from django.conf.urls import include,url
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d877ca33a0e342146d320beb8f51ec8ba8d08f89
     url(r'^main/', include('main.urls')),
-]
+    ]
 
-
-<<<<<<< HEAD
-=======
-    url(r'^about_sig/', include('about_sig.urls')),
-]
->>>>>>> cf5cbc2b5711cbff71253c61e37f596c59b7bfac
-=======
->>>>>>> d877ca33a0e342146d320beb8f51ec8ba8d08f89
