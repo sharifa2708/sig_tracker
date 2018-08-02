8b2681ad0f0fe3c41fbbc0468a188baff9ba391e
sharifa2708 <sanjay5803@gmail.com>
2018-08-03 01:55:48 +0530

    Removed redundant directories

--- sig_tracker/about_sig/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import Sig
-from . models import Topics
-
-admin.site.register(Sig)
-admin.site.register(Topics)
--- sig_tracker/dev_sig/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DevSigConfig(AppConfig):
-    name = 'dev_sig'
